0b15a75dd3be01c40501adf8ff1cb44f673551c2
Sececo <01sebas.co@gmail.com>
2026-02-20 16:21:20 -0500

    feat: configurar base de datos para que use el modelo Conductor

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,4 +1,6 @@
 import { Sequelize } from 'sequelize-typescript';
+import { ConductorModel } from '../models/conductor-model';
+
 const sequelize = new Sequelize({
     database: process.env.DB_NAME,
     dialect: 'postgres',
@@ -6,7 +8,7 @@ const sequelize = new Sequelize({
     port: Number(process.env.DB_PORT) || 5432,
     password: process.env.DB_PASSWORD,
     username: process.env.DB_USER,
-    models: [],
+    models: [ConductorModel],
 });
 
 export { sequelize };
